97d92b122e03adbe04f44f863d00a89871688399
Fang Ernesto <zxc9799979@gmail.com>
2016-12-25 19:20:26 +0800

    admin css ver 1.1

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,14 +9,14 @@ class Admin::UsersController < ApplicationController
 
     station_time_log = {}
 
-    # umbrella_rent_log.each do |u|
-    #   if u.start_location_id == 1
-    #     station_time_log[u.start_time] = {:in => u.start_location_id}
-    #   end
-    #   if u.end_location_id == 1
-    #     station_time_log[u.end_time] = {:out => u.end_location_id}
-    #   end
-    # end
+    umbrella_rent_log.each do |u|
+      if u.start_location_id == 1
+        station_time_log[u.start_time] = {:in => u.start_location_id}
+      end
+      if u.end_location_id == 1
+        station_time_log[u.end_time] = {:out => u.end_location_id}
+      end
+    end
 
     @station_time_log = station_time_log.sort_by { |key, value| key }
   end
